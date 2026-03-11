f2c478167336df1c74440afef4d6e9c153195af0
colelatour <clatour0@byu.edu>
2026-03-11 13:34:44 -0600

    Second Commit

--- backend/Mission10_LaTour/obj/Debug/net10.0/Mission10_LaTour.AssemblyInfo.cs
+++ backend/Mission10_LaTour/obj/Debug/net10.0/Mission10_LaTour.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission10_LaTour")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33955f1414fe616987d0de6e34ebc56e0484ca86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission10_LaTour")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission10_LaTour")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
